447fbcca4d5a583201bea0b14f2b5ff07addf42e
gdub22 <garth22@gmail.com>
2013-12-07 14:51:52 -0800

    upload api docs

---
+++ api/api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "RESTless",
+        "RESTless.Adapter",
+        "RESTless.Client",
+        "RESTless.FixtureAdapter",
+        "RESTless.JSONSerializer",
+        "RESTless.Model",
+        "RESTless.RESTAdapter",
+        "RESTless.ReadOnlyModel",
+        "RESTless.RecordArray",
+        "RESTless.Serializer",
+        "RESTless.State"
+    ],
+    "modules": [
+        "ember-restless"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-restless",
+            "name": "ember-restless"
+        }
+    ]
+} };
+});
