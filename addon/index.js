5a7befc0f9276056b20652255d580c63849a5d8a
Garth Poitras <garth22@gmail.com>
2015-05-09 15:34:45 -0400

    support importing restless components with ember-cli

---
+++ addon/index.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export var Client = RESTless.Client;
+export var Adapter = RESTless.Adapter;
+export var RESTAdapter = RESTless.RESTAdapter;
+export var attr = RESTless.attr;
+export var belongsTo = RESTless.belongsTo;
+export var hasMany = RESTless.hasMany;
+export var Model = RESTless.Model;
+export var ReadOnlyModel = RESTless.ReadOnlyModel;
+export var RecordArray = RESTless.RecordArray;
+export var Serializer = RESTless.Serializer;
+export var JSONSerializer = RESTless.JSONSerializer;
+export var Transform = RESTless.Transform;
+export var BooleanTransform = RESTless.BooleanTransform;
+export var NumberTransform = RESTless.NumberTransform;
+export var StringTransform = RESTless.StringTransform;
+export var DateTransform = RESTless.DateTransform;
+export var JSONTransforms = RESTless.JSONTransforms;
